bd2d8fed3322a6c2ddc63d74ae7e3608d3a3c67c
Donte Handy <donte.handy@gmail.com>
2024-12-15 13:45:44 -0400

    deep dive into api information/pre it2

--- lib/dmv_data_service.rb
+++ lib/dmv_data_service.rb
@@ -1,5 +1,6 @@
 require 'faraday' #faraday is a gem that allows you to make http requests
 require 'json' #json is a class in the ruby standard library
+require 'pry'
 
 class DmvDataService
   def load_data(source) #method to load data from a source
@@ -36,3 +37,37 @@ end
 #As detailed in the interaction pattern, you can access the data you need with the following code snippet.
 
 #DmvDataService.new.wa_ev_registrations
+
+#use as template for Handy Transport APP/ Dispatch idea.  Take Data from Routes -> populate in CF -> buy -> lean -> LS.automation with going rate from DATloadboard. 
+#handy transport LTL idea for finding ltl in between and base on cubes. 30 minutes. one picture. 
+#use as template....
+
+#to access from pry to answer 
+
+# How can we access data that is nested more deeply?
+
+#1 require 'pry' - check on the top of the file
+#2 require './lib/dmv_data_service' - to load the file, return should be true - q
+#3 service = DmvDataService.new, data = service.wa_ev_registrations - q
+#4 data.class - returns ARRAY #What is the datatype? 
+
+#What information is here? 
+#running with comma there is less information returned - 1 or 2 hybrid vehicles
+#running individually there is more information returned - EVERYTHING THAT THE FILE HAS
+
+
+# How can we access data that is nested more deeply?
+#since its an array, we can access the first element by using some simple methods to look around
+#data.first - returns the first element in the array
+#data.count - returns the number of elements in the array which is 1000
+#data.first.keys #returns an array of the keys in the first element of the array
+#data.first[:fuel_type] #returns the value of the fuel_type key in the first element of the array
+#data[0] #returns the first element in the array  #same as data.first q
+
+#same as any other array, you can access the elements by their index
+
+
+
+
+
+

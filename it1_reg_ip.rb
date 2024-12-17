41024f787eecea968acbe06069f328dc6d6a23b6
Donte Handy <donte.handy@gmail.com>
2024-12-17 19:33:38 -0400

    ran it1_reg_ip.rb - return: true

--- it1_reg_ip.rb
+++ it1_reg_ip.rb
@@ -2,9 +2,11 @@
 # By default, if any error occurs, the script will stop execution at that line, 
 # and Pry will return to the REPL, allowing you to debug interactively.
 
+#navigate to the_dmv directory
 
-# FILE: it1_reg_ip.rb
 #pry ->  load 'it1_reg_ip.rb'
+#if true is the return, all test passed
+
 
 require './lib/registrant'
 #=> true
@@ -15,15 +17,17 @@ registrant_1 = Registrant.new('Bruce', 18, true)
 registrant_2 = Registrant.new('Penny', 15)
 #=> #<Registrant:0x000000015c0778c8 @age=15, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Penny", @permit=false>
 
-registrant_1.name
+#the above initializes two instances of the Registrant class, registrant_1 and registrant_2
+
+registrant_1.name #method that will return the name attribute of the Registrant class
 #=> "Bruce"
 
-registrant_1.age
+registrant_1.age #method that will return the age attribute of the Registrant class
 #=> 18
 
 registrant_1.permit? #method for permit? 
 #that checks if attribute permit from Registrant class is true.
-#for registrant_1
+#for registrant_1 - was set to true during initialization 
 #=> true
 
 registrant_1.license_data #method that will return the key value pairs of @license data hash in Registrant class
@@ -48,4 +52,11 @@ registrant_2.earn_permit #is a an action method that will change the permit attr
 registrant_2.permit? #is a predicate method that will check if the permit attribute is true
 #=> true
 
+
+#last two change the hash of data in the @license_data instance variable
+#the method earn_permit changes the permit attribute to true
+#asking directly after if the permit attribute is true will return true
+
+
+#---series of checking for expected returns that may indicate that a function has been broken
 
